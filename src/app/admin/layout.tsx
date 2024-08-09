4a984f06ab33264707398f358dac5f1d296fed24
kubagrabski <jakub.grabski@grupa-abs.pl>
2024-08-09 20:47:46 +0200

    Added Admin layout, page, navigation, UI card

---
+++ src/app/admin/layout.tsx
@@ -0,0 +1,17 @@
+import { Nav, NavLink } from "@/components/Nav"
+
+export default function AdminLayout({
+    children,
+}: Readonly<{
+    children: React.ReactNode
+}>) {
+    return <>
+    <Nav>
+        <NavLink href="/admin">Dashboard</NavLink>
+        <NavLink href="/admin/cars">Cars</NavLink>
+        <NavLink href="/admin/users">Users</NavLink>
+        <NavLink href="/admin/orders">Orders</NavLink>
+    </Nav>
+    <div className="container my-6">{children}</div>
+    </>
+}
---
+++ src/app/admin/page.tsx
@@ -0,0 +1,97 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import db from "@/db/db";
+import { formatCurrency, formatNumber } from "@/lib/formatters";
+
+async function getSalesData() {
+  const data = await db.order.aggregate({
+    _sum: { pricePaidInCents: true },
+    _count: true,
+  });
+
+  return {
+    amount: (data._sum.pricePaidInCents || 0) / 100,
+    numberOfSales: data._count,
+  };
+}
+
+async function getUserData() {
+  const [userCount, orderData] = await Promise.all([
+    db.user.count(),
+    db.order.aggregate({
+      _sum: { pricePaidInCents: true },
+    }),
+  ]);
+
+  return {
+    userCount,
+    averageValuePerUser:
+      userCount === 0
+        ? 0
+        : (orderData._sum.pricePaidInCents || 0) / userCount / 100,
+  };
+}
+
+async function getCarData() {
+  const [activeCount, inactiveCount] = await Promise.all([
+    db.car.count({ where: { isAvailableForPurchase: true } }),
+    db.car.count({ where: { isAvailableForPurchase: false } }),
+  ]);
+
+  return { activeCount, inactiveCount };
+}
+
+export default async function AdminDashboard() {
+  const [salesData, userData, carData] = await Promise.all([
+    getSalesData(),
+    getUserData(),
+    getCarData(),
+  ]);
+
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+      <DashboardCard
+        title="Sales"
+        subtitle={`${formatNumber(salesData.numberOfSales)} Orders`}
+        body={formatCurrency(salesData.amount)}
+      />
+      <DashboardCard
+        title="Customers"
+        subtitle={`${formatCurrency(
+          userData.averageValuePerUser
+        )} Average value`}
+        body={formatNumber(userData.userCount)}
+      />
+      <DashboardCard
+        title="Active Cars"
+        subtitle={`${formatNumber(carData.inactiveCount)} Inactive`}
+        body={formatNumber(carData.activeCount)}
+      />
+    </div>
+  );
+}
+
+type DashboardCardProps = {
+  title: string;
+  subtitle: string;
+  body: string;
+};
+
+function DashboardCard({ title, subtitle, body }: DashboardCardProps) {
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>{title}</CardTitle>
+        <CardDescription>{subtitle}</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <p>{body}</p>
+      </CardContent>
+    </Card>
+  );
+}
