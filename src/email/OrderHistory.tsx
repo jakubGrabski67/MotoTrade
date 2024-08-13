8422cf3e885adf0bf61f7e684d11c42ca30a24c0
kubagrabski <jakub.grabski@grupa-abs.pl>
2024-08-13 14:09:28 +0200

    Added orders history sending email

---
+++ src/email/OrderHistory.tsx
@@ -0,0 +1,81 @@
+import {
+  Body,
+  Container,
+  Head,
+  Heading,
+  Hr,
+  Html,
+  Preview,
+  Tailwind,
+} from "@react-email/components";
+import { OrderInformation } from "./components/OrderInformation";
+import React from "react";
+
+type OrderHistoryEmailProps = {
+  orders: {
+    id: string;
+    pricePaidInCents: number;
+    createdAt: Date;
+    downloadVerificationId: string;
+    car: {
+      name: string;
+      imagePath: string;
+      description: string;
+    };
+  }[];
+};
+
+OrderHistoryEmail.PreviewProps = {
+  orders: [
+    {
+      id: crypto.randomUUID(),
+      createdAt: new Date(),
+      pricePaidInCents: 10000,
+      downloadVerificationId: crypto.randomUUID(),
+      car: {
+        name: "Car name",
+        description: "Some description",
+        imagePath:
+          "/cars/b21a35c7-ea18-4a21-8cdb-d8ad425e2f41-449729027_3637037673273489_905451309854713318_n.jpg",
+      },
+    },
+    {
+      id: crypto.randomUUID(),
+      createdAt: new Date(),
+      pricePaidInCents: 2000,
+      downloadVerificationId: crypto.randomUUID(),
+      car: {
+        name: "Car name 2",
+        description: "Some other desc",
+        imagePath:
+          "/cars/b21a35c7-ea18-4a21-8cdb-d8ad425e2f41-449729027_3637037673273489_905451309854713318_n.jpg",
+      },
+    },
+  ],
+} satisfies OrderHistoryEmailProps;
+
+export default function OrderHistoryEmail({ orders }: OrderHistoryEmailProps) {
+  return (
+    <Html>
+      <Preview>Order History & Downloads</Preview>
+      <Tailwind>
+        <Head />
+        <Body className="font-sans bg-white">
+          <Container className="max-w-xl">
+            <Heading>Order History</Heading>
+            {orders.map((order, index) => (
+              <React.Fragment key={order.id}>
+                <OrderInformation
+                  order={order}
+                  car={order.car}
+                  downloadVerificationId={order.downloadVerificationId}
+                />
+                {index < orders.length - 1 && <Hr />}
+              </React.Fragment>
+            ))}
+          </Container>
+        </Body>
+      </Tailwind>
+    </Html>
+  );
+}
